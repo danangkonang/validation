2f00b19d9899549e64eb3b8a3e91529026ae753b
danangkonang <dngrifai21@gmail.com>
2021-06-11 10:03:44 +0700

    init repo

---
+++ validation.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"errors"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func Validation(data, rules string) error {
+	rule := strings.Split(rules, "|")
+	for _, r := range rule {
+
+		// required
+		if r == "required" {
+			if data == "" {
+				return errors.New("kosong")
+			}
+		}
+
+		// min length
+		min := regexp.MustCompile("minlength").MatchString(r)
+		if min {
+			min_length := strings.Split(r, ":")[1]
+			length_minimum, _ := strconv.Atoi(min_length)
+			length_value := len(data)
+
+			if length_value < length_minimum {
+				return errors.New("minlength is " + min_length)
+			}
+		}
+
+		// max length
+		max := regexp.MustCompile("maxlength").MatchString(r)
+		if max {
+			max_length := strings.Split(r, ":")[1]
+			length_maximum, _ := strconv.Atoi(max_length)
+			length_value := len(data)
+
+			if length_value > length_maximum {
+				return errors.New("maxlength is " + max_length)
+			}
+		}
+
+		// is email address
+		if r == "email" {
+			emailRegexString1 := "^(?:(?:(?:(?:[a-zA-Z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])+(?:\\.([a-zA-Z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])+)*)|(?:(?:\\x22)(?:(?:(?:(?:\\x20|\\x09)*(?:\\x0d\\x0a))?(?:\\x20|\\x09)+)?(?:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x7f]|\\x21|[\\x23-\\x5b]|[\\x5d-\\x7e]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(?:(?:[\\x01-\\x09\\x0b\\x0c\\x0d-\\x7f]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}]))))*(?:(?:(?:\\x20|\\x09)*(?:\\x0d\\x0a))?(\\x20|\\x09)+)?(?:\\x22))))@(?:(?:(?:[a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(?:(?:[a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])(?:[a-zA-Z]|\\d|-|\\.|~|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])*(?:[a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])))\\.)+(?:(?:[a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(?:(?:[a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])(?:[a-zA-Z]|\\d|-|\\.|~|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])*(?:[a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])))\\.?$"
+			re := regexp.MustCompile(emailRegexString1)
+			isEmail := re.MatchString(data)
+			if !isEmail {
+				return errors.New("invalid email address")
+			}
+		}
+
+	}
+	return nil
+}
