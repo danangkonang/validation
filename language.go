32ef2b5c3ca4f6c8ea2ab9e4a5c67901b3c06b4e
danangkonang <danangkonang21@gmail.com>
2023-01-28 00:52:58 +0700

    create global language

---
+++ language.go
@@ -0,0 +1,23 @@
+package validation
+
+var (
+	Lang = map[string]string{
+		"required":  "This field is required",
+		"alpha":     "This field must be a valid alpha",
+		"alphanum":  "This field must be a valid alphanum",
+		"number":    "This field must be a valid number",
+		"numeric":   "This field must be a valid numeric",
+		"email":     "This field must be a valid email address",
+		"latitude":  "This field must be a valid latitude",
+		"longitude": "This field must be a valid longitude",
+		"ip":        "This field must be a valid ip",
+		"boolean":   "This field must be a valid boolean",
+		"ipv4":      "This field must be a valid ipv4",
+		"ipv6":      "This field must be a valid ipv6",
+		"url":       "This field must be a valid url",
+		"date":      "This field must be a valid date",
+		"min":       "This field must be a minimum {{.}}",
+		"max":       "This field must be a maximum {{.}} charakter",
+		"eqfield":   "This field must be a equal with {{.}}",
+	}
+)
